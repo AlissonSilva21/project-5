e8345dae72ec83060bb54b099a48ccde2b9daa21
leonardograndi <leonardofelipegrandi@gmail.com>
2018-02-15 18:05:36 -0200

    Normalizando porta servidor de App

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const http = require('http');
 const debug = require('debug')('project5:server');
 const express = require('express');
 
-
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
+
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -19,8 +19,21 @@ const route = router.get('/', (req, res, next) => {
         version: "0.0.1"
     });
 });
-
 app.use('/', route);
 
 server.listen(port);
-console.log('foi caraleo');
+
+function normalizePort(val) {
+
+    const port = parseInt(val, 10);
+
+    if(isNaN(port)) {
+        return val;
+    }
+
+    if(port >= 0) {
+        return port;
+    }
+
+    return false;
+}
