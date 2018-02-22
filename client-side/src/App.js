f610caab8f2c7ea15f5f4557d1b01c8fa2b0b4c4
Unknown <leonardofelipegrandi@gmail.com>
2018-02-22 17:25:29 -0300

    Install ReactJS + Separating NodeJS Server

---
+++ client-side/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+export default class App extends Component {
+
+
+  render(){
+    return(
+      <div>
+        <h2>SMARTCITIES - JOKENPO.IO</h2>
+      </div>
+    );
+  }
+}
