f54330f6c346b5f4ca2d4a21c617ff904015de75
Unknown <contato@infoobra.com.br>
2018-02-27 19:13:37 -0300

    Find Players + Rank TOP10

--- server-side/src/controllers/controllerApplication.js
+++ server-side/src/controllers/controllerApplication.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
 const authMiddleware = require('../middlewares/auth');
 
@@ -7,7 +8,11 @@ const authMiddleware = require('../middlewares/auth');
 router.use(authMiddleware);
 
 router.get('/rank', (req, res)=>{
-    return res.send({ ok: true });
+
+    User.find({ 'nickname': { $not: { $type: 10 } } })
+        .sort('-score')
+        .then( data =>  res.status(200).send(data))
+        .catch( err => res.status(400).send(e))
 });
 
 module.exports = app => app.use('/game', router);
